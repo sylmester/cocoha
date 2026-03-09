c8d5860f02fb5a0c57ec8ae7635fefd81ec26acd
Sylvester <90195024+sylmester@users.noreply.github.com>
2026-03-09 17:48:36 +0100

    Refactor code to simplify TRF into groups and improve naming. Added Extra preprocessing steps in load-and_preprocess-eeg.ipynb.

---
+++ analysis/trf_functions.py
@@ -0,0 +1,17 @@
+import file_paths
+import eelbrain
+
+trf_types = ["attended", 
+        "unattended", 
+        "attended_padded", 
+        "unattended_padded", 
+        "attended_padded_decoder", 
+        "unattended_padded_decoder",
+        "attended_decoder",
+        "unattended_decoder",
+        "attended_decoder_50ms_basis",
+        "unattended_decoder_50ms_basis",
+        "attended_log_8_band",
+        "unattended_log_8_band",
+        "attended_preprocessed_eeg",
+        "unattended_preprocessed_eeg"]
